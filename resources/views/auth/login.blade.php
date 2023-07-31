3cea37a768b93863f092323f4b4f31af3942349a
asakura <asakura@asasoft.info>
2023-08-01 01:52:19 +0900

    ログイン画面、背景画像、ランダム表示対応

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,1 +1,7 @@
 @extends('adminlte::auth.login')
+
+@section('adminlte_css_pre')
+    <link rel="stylesheet" href="{{ asset('/css/style.css') }}">
+@stop
+
+@section('classes_body'){{ 'login-page login-background-image-' . random_int(1, 4) }}@stop
